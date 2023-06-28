da24a90f2c3e6ba928d98d218dc8940747aa772f
莊翔淯 (Xiang-Yu, Zhuang) <55974558+xiangyu066@users.noreply.github.com>
2023-06-28 16:02:12 +0800

    Add files via upload

--- MSD_Example.py
+++ MSD_Example.py
@@ -42,19 +42,19 @@ if __name__ == '__main__':
     
     # calculate eMSD by for-loop
     tau,eMSD_for=msd.eMSD_calc(t,xx,yy,'for-loop')
-    slope,eMSD_for_f=msd.MSD_fitting(tau,eMSD_for,'MSD_fit')
-    polyorder,eMSD_for_f_log=msd.MSD_fitting(tau,eMSD_for,'loglog_fit')
+    slope,eMSD_for_fit=msd.MSD_fitting(tau,eMSD_for,'MSD_fit')
+    polyorder,eMSD_for_fit_log=msd.MSD_fitting(tau,eMSD_for,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,eMSD_for,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,eMSD_for_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,eMSD_for_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,eMSD_for,Label='Simulation')
-    axes[1].plot(tau,eMSD_for_f_log,'--',Label='fitting')
+    axes[1].plot(tau,eMSD_for_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
@@ -66,19 +66,19 @@ if __name__ == '__main__':
 
     # calculate eMSD by vectorization
     tau,eMSD_vect=msd.eMSD_calc(t,xx,yy,'vectorization')
-    slope,eMSD_vect_f=msd.MSD_fitting(tau,eMSD_vect,'MSD_fit')
-    polyorder,eMSD_vect_f_log=msd.MSD_fitting(tau,eMSD_vect,'loglog_fit')
+    slope,eMSD_vect_fit=msd.MSD_fitting(tau,eMSD_vect,'MSD_fit')
+    polyorder,eMSD_vect_fit_log=msd.MSD_fitting(tau,eMSD_vect,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,eMSD_vect,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,eMSD_vect_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,eMSD_vect_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,eMSD_vect,Label='Simulation')
-    axes[1].plot(tau,eMSD_vect_f_log,'--',Label='fitting')
+    axes[1].plot(tau,eMSD_vect_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
@@ -90,19 +90,19 @@ if __name__ == '__main__':
 
     # calculate time-averaged eMSD by for-loop
     tau,time_eMSD_for=msd.time_averaged_eMSD_calc(t,xx,yy,'for-loop')
-    slope,time_eMSD_for_f=msd.MSD_fitting(tau,time_eMSD_for,'MSD_fit')
-    polyorder,time_eMSD_for_f_log=msd.MSD_fitting(tau,time_eMSD_for,'loglog_fit')
+    slope,time_eMSD_for_fit=msd.MSD_fitting(tau,time_eMSD_for,'MSD_fit')
+    polyorder,time_eMSD_for_fit_log=msd.MSD_fitting(tau,time_eMSD_for,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,time_eMSD_for,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,time_eMSD_for_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,time_eMSD_for_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('time-averaged eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,time_eMSD_for,Label='Simulation')
-    axes[1].plot(tau,time_eMSD_for_f_log,'--',Label='fitting')
+    axes[1].plot(tau,time_eMSD_for_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
@@ -114,19 +114,19 @@ if __name__ == '__main__':
 
     # calculate time-averaged eMSD by vectorization
     tau,time_eMSD_vect=msd.time_averaged_eMSD_calc(t,xx,yy,'vectorization')
-    slope,time_eMSD_vect_f=msd.MSD_fitting(tau,time_eMSD_vect,'MSD_fit')
-    polyorder,time_eMSD_vect_f_log=msd.MSD_fitting(tau,time_eMSD_vect,'loglog_fit')
+    slope,time_eMSD_vect_fit=msd.MSD_fitting(tau,time_eMSD_vect,'MSD_fit')
+    polyorder,time_eMSD_vect_fit_log=msd.MSD_fitting(tau,time_eMSD_vect,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,time_eMSD_vect,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,time_eMSD_vect_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,time_eMSD_vect_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('time-averaged eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,time_eMSD_vect,Label='Simulation')
-    axes[1].plot(tau,time_eMSD_vect_f_log,'--',Label='fitting')
+    axes[1].plot(tau,time_eMSD_vect_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
@@ -138,19 +138,19 @@ if __name__ == '__main__':
     
     # calculate time-averaged eMSD by parallel computing
     tau,time_eMSD_parallel=msd.time_averaged_eMSD_calc(t,xx,yy,'parallel')
-    slope,time_eMSD_parallel_f=msd.MSD_fitting(tau,time_eMSD_parallel,'MSD_fit')
-    polyorder,time_eMSD_parallel_f_log=msd.MSD_fitting(tau,time_eMSD_parallel,'loglog_fit')
+    slope,time_eMSD_parallel_fit=msd.MSD_fitting(tau,time_eMSD_parallel,'MSD_fit')
+    polyorder,time_eMSD_parallel_fit_log=msd.MSD_fitting(tau,time_eMSD_parallel,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,time_eMSD_parallel,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,time_eMSD_parallel_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,time_eMSD_parallel_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('time-averaged eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,time_eMSD_parallel,Label='Simulation')
-    axes[1].plot(tau,time_eMSD_parallel_f_log,'--',Label='fitting')
+    axes[1].plot(tau,time_eMSD_parallel_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
@@ -162,19 +162,19 @@ if __name__ == '__main__':
 
     # calculate time-averaged eMSD by gpu
     tau,time_eMSD_gpu=msd.time_averaged_eMSD_calc(t,xx,yy,'gpu')
-    slope,time_eMSD_gpu_f=msd.MSD_fitting(tau,time_eMSD_gpu,'MSD_fit')
-    polyorder,time_eMSD_gpu_f_log=msd.MSD_fitting(tau,time_eMSD_gpu,'loglog_fit')
+    slope,time_eMSD_gpu_fit=msd.MSD_fitting(tau,time_eMSD_gpu,'MSD_fit')
+    polyorder,time_eMSD_gpu_fit_log=msd.MSD_fitting(tau,time_eMSD_gpu,'loglog_fit')
     
     fig,axes=plt.subplots(1,2,figsize=(8,6))
     axes[0].plot(tau,time_eMSD_gpu,label="data(nParticles="+str(nParticles)+")")
-    axes[0].plot(tau,time_eMSD_gpu_f,'r--',lw=1,label='fitting')
+    axes[0].plot(tau,time_eMSD_gpu_fit,'r--',lw=1,label='fitting')
     axes[0].grid(linewidth=0.3)
     axes[0].legend()
     axes[0].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[0].set_ylabel('time-averaged eMSD [$\mu m^2$]',fontweight='bold')
 
     axes[1].plot(tau,time_eMSD_gpu,Label='Simulation')
-    axes[1].plot(tau,time_eMSD_gpu_f_log,'--',Label='fitting')
+    axes[1].plot(tau,time_eMSD_gpu_fit_log,'--',Label='fitting')
     axes[1].set_xlabel('$\Delta t$ [sec]',fontweight='bold')
     axes[1].grid(lw=0.3)
     axes[1].set_yscale('log')
